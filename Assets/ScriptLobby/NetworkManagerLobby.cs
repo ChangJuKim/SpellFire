0c681e3c58596f562b993ec461b644b702eb74bd
Riantix <changjukim24@gmail.com>
2025-05-12 12:56:34 -0400

    Added lobby features

---
+++ Assets/ScriptLobby/NetworkManagerLobby.cs
@@ -0,0 +1,70 @@
+using Mirror;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerLobby : NetworkManager
+{
+    [Scene] [SerializeField] private string menuScene = string.Empty;
+
+    [Header("Room")]
+    [SerializeField] private NetworkRoomPlayerSpellFire roomPlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+
+    public override void OnStartServer()
+    {
+        // Load spawnable prefabs here
+    }
+
+    public override void OnStartClient()
+    {
+        // Load spawnable prefabs here
+        var spawnablePrefabs = new GameObject[0];
+
+        foreach (var prefab in spawnablePrefabs)
+        {
+            NetworkClient.RegisterPrefab(prefab);
+        }
+    }
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+
+        OnClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+
+        OnClientDisconnected?.Invoke();
+    }
+
+    public override void OnServerConnect(NetworkConnectionToClient conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().name != menuScene)
+        {
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        if (SceneManager.GetActiveScene().name == menuScene)
+        {
+            NetworkRoomPlayerSpellFire roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+}
---
+++ Assets/ScriptLobby/PlayerNameInput.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+
+    public static string DisplayName { get; private set; }
+
+    private const string PLAYER_PREFS_NAME_KEY = "PlayerName";
+
+    private void Start()
+    {
+        SetUpInputField();
+    }
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PLAYER_PREFS_NAME_KEY))
+        {
+            return;
+        }
+
+        string defaultName = PlayerPrefs.GetString(PLAYER_PREFS_NAME_KEY);
+
+        nameInputField.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        DisplayName = nameInputField.text;
+
+        PlayerPrefs.SetString(PLAYER_PREFS_NAME_KEY, name);
+    }
+}
