554cecba3c88c0e6618106771b951a26e75341c6
Riantix <changjukim24@gmail.com>
2025-06-01 00:28:24 -0400

    Debugged issue

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -80,7 +80,7 @@ namespace QuickStart
 
         void Update()
         {
-            if (!hasAuthority)
+            if (!isOwned)
             {
                 // make non-local players run this
                 floatingInfo.transform.LookAt(Camera.main.transform);
